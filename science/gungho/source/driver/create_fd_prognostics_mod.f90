da6f36757398ac8a008378048e3e870f42cf5e98
iboutle <135141261+iboutle@users.noreply.github.com>
2026-07-01 15:31:00 +0100

    Squashed commit of the following:
    commit d4624bea443a8d24a34ad713a77a24eb8d8b0800
    Author: Tim Graham <tim.graham@metoffice.gov.uk>
    Date:   Thu Apr 23 15:48:25 2026 +0100
    
        Add lf_inland_flow to the lfric_dictionary
    
    commit a84d988ad1a418d24f611c7674543fd60f995e99
    Author: Tim Graham <tim.graham@metoffice.gov.uk>
    Date:   Tue Apr 14 09:40:31 2026 +0100
    
        Merged in timgraham-Met/vn3.1_inland_flow
    
    commit 09d8e6df1716f4999fe65d98840ba8ff585dc58f
    Author: Tim Graham <tim.graham@metoffice.gov.uk>
    Date:   Tue Apr 14 09:35:08 2026 +0100
    
        Merged in tag3.1_snowinc_jules
    
    commit b599f1c050b78d371549f33799594c644bb1410d
    Author: Tim Graham <tim.graham@metoffice.gov.uk>
    Date:   Tue Apr 14 09:32:19 2026 +0100
    
        Merge in tag3.1_ostia_ice_ancils. Note that this upgrades to 2026.3.2 but shouldn't affect anything in GC6
    
    commit dda16ee971dc229ee2381d2bad223ab792319643
    Author: Tim Graham <tim.graham@metoffice.gov.uk>
    Date:   Tue Apr 14 09:21:55 2026 +0100
    
        Merged in tag3.1_sst_anomaloy

--- science/gungho/source/driver/create_fd_prognostics_mod.f90
+++ science/gungho/source/driver/create_fd_prognostics_mod.f90
@@ -30,6 +30,8 @@ module create_fd_prognostics_mod
                                              read_w2h_wind,               &
                                              sst_source,                  &
                                              sst_source_start_dump,       &
+                                             sea_ice_source,              &
+                                             sea_ice_source_start_dump,   &
                                              init_exner_method,           &
                                              init_exner_method_dump
   use nlsizes_namelist_mod,           only : sm_levels
@@ -275,7 +277,7 @@ contains
 
       ! For coupled models get the sea ice fraction and thickness from the
       ! dump
-      if (l_couple_sea_ice) then
+      if (l_couple_sea_ice .and. (sea_ice_source == sea_ice_source_start_dump)) then
          call setup_ancil_field("sea_ice_fraction", depository, &
                              fd_field_collection, mesh, twod_mesh, &
                              twod=.true., ndata=n_sea_ice_tile)
